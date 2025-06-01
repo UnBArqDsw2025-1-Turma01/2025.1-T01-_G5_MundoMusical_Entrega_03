885e9e2d7366b70212522d225a77518f3ce06ccf
arthur-heleno <arthur-heleno@hotmail.com>
2025-06-01 18:25:52 -0300

    Update proxy.js
    Atualização de proxy.js com exemplo funcionando
    
    Co-Authored-By: EstherSCS <91230616+EstherSousa@users.noreply.github.com>
    Co-Authored-By: Laís Soares <92321749+Laisczt@users.noreply.github.com>

--- docs/PadroesDeProjeto/proxy/proxy.js
+++ docs/PadroesDeProjeto/proxy/proxy.js
@@ -1,184 +1,118 @@
-interface CorrecaoExercicio {
-    corrigir (nota: Number);
-    gerarFeedback(idFeedback: Number, comentario: String) : Feedback;
-}
-
-class ProxyExercicio implements CorrecaoExercicio{
-    private exercicio: Exercicio;
-
-    private negarSecao: boolean = false;
-    
-    constructor(exercicio : Exercicio) {
+var ProxyExercicio = /** @class */ (function () {
+    function ProxyExercicio(exercicio) {
+        this.negarSecao = false;
         this.exercicio = exercicio;
     }
-
-    corrigir(nota: Number) {
+    ProxyExercicio.prototype.corrigir = function (nota) {
         if (this.checkarSessao()) {
             this.exercicio.corrigir(nota);
             return;
         }
         throw Error("Não foi possivel verificar a seção");
-    }
-
-    gerarFeedback(idFeedback: Number, comentario: String) : Feedback{
-        if(this.checkarSessao()){
+    };
+    ProxyExercicio.prototype.gerarFeedback = function (idFeedback, comentario) {
+        if (this.checkarSessao()) {
             return this.exercicio.gerarFeedback(idFeedback, comentario);
         }
         throw Error("Não foi possivel verificar a seção");
-    }
-
-    private checkarSessao() : boolean {
+    };
+    ProxyExercicio.prototype.checkarSessao = function () {
         // spoof
         return !this.negarSecao;
-    }
-
-    public debugNegarSecao(valor : boolean){
+    };
+    ProxyExercicio.prototype.debugNegarSecao = function (valor) {
         this.negarSecao = valor;
-    }
-}
-
-class Exercicio implements CorrecaoExercicio {
-    private idExercicio: Number;
-    private idAutor: Number;  
-    private pontuacao: Number;
-    private dificuldade: Number;
-
-    constructor(idExercicio, idAutor, dificuldade){
+    };
+    return ProxyExercicio;
+}());
+var Exercicio = /** @class */ (function () {
+    function Exercicio(idExercicio, idAutor, dificuldade) {
         this.idExercicio = idExercicio;
         this.idAutor = idAutor;
         this.pontuacao = 0;
         this.dificuldade = dificuldade;
     }
-    
-    corrigir (nota){
+    Exercicio.prototype.corrigir = function (nota) {
         //spoof
         this.pontuacao = nota;
         return true;
-    }
-    
-    gerarFeedback (idFeedback, comentario) : Feedback {
+    };
+    Exercicio.prototype.gerarFeedback = function (idFeedback, comentario) {
         // spoof
         return new Feedback(idFeedback, this.idExercicio, this.pontuacao, comentario);
-    }
-
-    getPontuacao(){
+    };
+    Exercicio.prototype.getPontuacao = function () {
         return this.pontuacao;
-    }
-
-    // resto da implementacao do Exercício
-}
-
-class Feedback {
-    private idFeedback: Number;
-    private idExercicio: Number;
-    private codigoAluno: Number;
-    private data: String;
-    private mensagem: String;
-    private comentario: String;
-    private pontuacao: Number;
-
-    constructor(idFeedback: Number, idExercicio: Number, pontuacao: Number, comentario: String){
+    };
+    return Exercicio;
+}());
+var Feedback = /** @class */ (function () {
+    function Feedback(idFeedback, idExercicio, pontuacao, comentario) {
         this.idFeedback = idFeedback;
         this.idExercicio = idExercicio;
         this.pontuacao = pontuacao;
         this.comentario = comentario;
     }
-}
-
-
-class TesteCorrecao {
-    exercicio = new Exercicio(300, 5000, 3);
-    
-    testar(){
+    return Feedback;
+}());
+var TesteCorrecao = /** @class */ (function () {
+    function TesteCorrecao() {
+        this.exercicio = new Exercicio(300, 5000, 3);
+    }
+    TesteCorrecao.prototype.testar = function () {
         //simulando correção automática do exercicio
-        (this.exercicio as CorrecaoExercicio).corrigir(60);
-
+        this.exercicio.corrigir(60);
         console.log("Pontuacao automática do exercício: " + this.exercicio.getPontuacao());
-
         // Superscrição do professor
-        let correcao = new ProxyExercicio(this.exercicio);
-
+        var correcao = new ProxyExercicio(this.exercicio);
         correcao.corrigir(80);
-
         console.log("Pontuacao após correção do professor: " + this.exercicio.getPontuacao());
-
         // teste com seção inválida:
         console.log("Tentativa de correcao com seção inválida: ");
-
         correcao.debugNegarSecao(true);
         try {
             correcao.corrigir(100);
-        } catch (e) {
+        }
+        catch (e) {
             console.log(e);
         }
         console.log("Pontuação: " + this.exercicio.getPontuacao());
-        
-    }
-}
-
-let teste2 = new TesteCorrecao();
+    };
+    return TesteCorrecao;
+}());
+var teste2 = new TesteCorrecao();
 teste2.testar();
-
 // class Quiz extends Exercicio {
 //     tema;
 //     resposta;
-
 //     inserirQuestao () {
-
 //     }
-
 //     excluirQuestao () {
-
 //     }
-
 //     editarQuestao () {
-
 //     }
-
 //     compararRespostas (resposta) {
-
 //     }
-
 //     gerarAlternativas (tema) {
-
 //     }
-
-
 // }
-
 // class ReconhecimentoDeAudio extends Exercicio {
 //     partitura;
 //     resposta;
-
 //     compararRespostas (resposta) {
-
 //     }
-    
-
 // }
-
 // class LeituraDePartituras extends Exercicio {
 //     partitura;
 //     resposta;
-
 //     compararRespostas (resposta) {
-
 //     }
-    
-
 // }
-
-
 // class DesafioDeRitmo extends Exercicio {
 //     precisao;
 //     partitura;
-
 //     gerarNotas () {
-
 //     }
 //     compararCliques () {
-
 //     }
-    
-
 // }
